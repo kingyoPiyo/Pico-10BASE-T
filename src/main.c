935cb52f5e15a2331a2e1c5aa222804af7c1b762
kingyo <kingyo.no.esa1192@gmail.com>
2022-09-02 23:07:47 +0900

    Clean main.c

--- src/main.c
+++ src/main.c
@@ -33,7 +33,6 @@ int main() {
     uint offset;
     PIO pio_ser_wr = pio0;
     uint sm0 = 0;
-    uint32_t data_32b;
     uint32_t lp_cnt = 0;
 
     stdio_init_all();
@@ -47,37 +46,37 @@ int main() {
 
 
     // 10BASE-T Serializer PIO init
-    // sideset を使う都合上、GPIOピンは連番である必要がある。16,17を使用する。
+    // Pin numbers must be sequential. (use pin16, 17)
     offset = pio_add_program(pio_ser_wr, &ser_10base_t_program);
     ser_10base_t_program_init(pio_ser_wr, sm0, offset, 16);
 
 
-    // 最初にNLPを送って対向機器に10BASE-T半二重であることを認識させる
-    for (uint32_t nlp = 0, lp = 0; nlp < 200;) {
-        // リンクパルス周期は16ms, ループ周期は0.8us
-        // 16ms / 0.8us = 20000
-        if (++lp == 20000) {
-            lp = 0;
-            nlp++;
-            data_32b = 0x0000000A;  // High(パルス幅100ns)
-        } else {
-            data_32b = 0x00000000;  // IDLE
-        }
-        ser_10base_t_tx_10b(pio_ser_wr, sm0, data_32b);
+    // Wait for Link up....
+    for (uint32_t i = 0; i < 100; i++) {
+        // Sending NLP Pulse (Pulse width = 100ns)
+        ser_10base_t_tx_10b(pio_ser_wr, sm0, 0x0000000A);
+        // NLP interval = 16ms +/- 8ms
+        sleep_ms(20);
     }
 
 
-    // パケット送出間隔が伸びる場合はリンクを維持するためにNLPの発行が必要だが、
-    // 20ms周期で送る場合はなくても良いみたい。
-    while (1) {
-        lp_cnt++;
-        sprintf(udp_payload, "Hello World!! Raspico 10BASE-T !! lp_cnt:%d", lp_cnt);
+    // Send packets every about 200ms.
+    for (uint32_t i = 0;;i++) {
 
-        udp_packet_gen_10base(tx_buf_udp, udp_payload);
-        for (uint32_t i = 0; i < DEF_UDP_BUF_SIZE+1; i++) {
-            ser_10base_t_tx_10b(pio_ser_wr, sm0, tx_buf_udp[i]);
+        if ((i % 10) == 0) {
+
+            // Sending UDP packets
+            sprintf(udp_payload, "Hello World!! Raspico 10BASE-T !! lp_cnt:%d", lp_cnt++);
+            udp_packet_gen_10base(tx_buf_udp, udp_payload);
+            for (uint32_t i = 0; i < DEF_UDP_BUF_SIZE+1; i++) {
+                ser_10base_t_tx_10b(pio_ser_wr, sm0, tx_buf_udp[i]);
+            }
+
+        } else {
+            // Sending NLP Pulse (Pulse width = 100ns)
+            ser_10base_t_tx_10b(pio_ser_wr, sm0, 0x0000000A);
         }
-
+        
         sleep_ms(20);
     }
 }
