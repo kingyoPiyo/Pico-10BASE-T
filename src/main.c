ff2393b8730f38a4c88ee71b762e24169e8e193d
kingyo <kingyo.no.esa1192@gmail.com>
2022-11-15 00:23:49 +0900

    Improved main loop for ease of use.

--- src/main.c
+++ src/main.c
@@ -10,8 +10,13 @@
 #include "udp.h"
 #include "hardware/pio.h"
 #include "ser_10base_t.pio.h"
+#include <stdio.h>
 
-#define HW_PINNUM_LED0      (25)    // Pico onboard LED
+
+#define HW_PINNUM_LED0      (25)        // Pico onboard LED
+#define DEF_NLP_INTERVAL_US (16000)     // NLP interval (16ms +/- 8ms)
+#define DEF_TX_INTERVAL_US  (200000)    // Dummy Data TX interval
+
 
 static struct repeating_timer timer;
 
@@ -34,6 +39,9 @@ int main() {
     PIO pio_ser_wr = pio0;
     uint sm0 = 0;
     uint32_t lp_cnt = 0;
+    uint32_t time_now = 0;
+    uint32_t time_nlp = 0;
+    uint32_t time_tx = 0;
 
     stdio_init_all();
     udp_init();
@@ -56,27 +64,36 @@ int main() {
         // Sending NLP Pulse (Pulse width = 100ns)
         ser_10base_t_tx_10b(pio_ser_wr, sm0, 0x0000000A);
         // NLP interval = 16ms +/- 8ms
-        sleep_ms(20);
+        sleep_ms(16);
     }
 
 
+    // Main loop
     // Send packets every about 200ms.
-    for (uint32_t i = 0;;i++) {
+    while (1) {
+        time_now = time_us_32();
 
-        if ((i % 10) == 0) {
 
-            // Sending UDP packets
+        // Sending NLP Puls
+        if ((time_now - time_nlp) > DEF_NLP_INTERVAL_US) {
+            time_nlp = time_now;
+            ser_10base_t_tx_10b(pio_ser_wr, sm0, 0x0000000A);
+        }
+
+
+        // Sending UDP packets
+        if ((time_now - time_tx) > DEF_TX_INTERVAL_US) {
+            time_tx = time_now;
             sprintf(udp_payload, "Hello World!! Raspico 10BASE-T !! lp_cnt:%d", lp_cnt++);
             udp_packet_gen_10base(tx_buf_udp, udp_payload);
             for (uint32_t i = 0; i < DEF_UDP_BUF_SIZE+1; i++) {
                 ser_10base_t_tx_10b(pio_ser_wr, sm0, tx_buf_udp[i]);
             }
-
-        } else {
-            // Sending NLP Pulse (Pulse width = 100ns)
-            ser_10base_t_tx_10b(pio_ser_wr, sm0, 0x0000000A);
         }
-        
-        sleep_ms(20);
+
+
+        /****  Put your code here ****/
+
     }
+
 }
