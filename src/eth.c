bbe8acf52eabe5d8d1ce773d7c3a17fe0338c7bb
kingyo <kingyo.no.esa1192@gmail.com>
2022-11-24 03:08:11 +0900

    Link Down 検出テスト

--- src/eth.c
+++ src/eth.c
@@ -19,6 +19,7 @@
 #define HW_PINNUM_OUT1          (0)             // SMA OUT0 for Debug
 #define DEF_NFLP_INTERVAL_US    (16000)         // NLP/FLP interval = 16ms +/- 8ms
 #define DEF_DMY_INTERVAL_US     (1000000)       // Dummy Data send interval
+#define DEF_LINK_TIMEOUT_US     (1000000)       // Link down time out
 
 #define DEF_ETHTYPE_IPV4        (0x0800)        // EtherType : IPv4
 #define DEF_ETHTYPE_ARP         (0x0806)        // EtherType : ARP
@@ -213,59 +214,68 @@ static void __time_critical_func(_rx_isr)(void) {
     uint8_t shift_num;
     uint32_t index = 0;
     uint32_t slot = 0;   // 0~3
+    uint32_t ldm_timer = time_us_32();
+    
     
     while(1) {
-        rx_buf = pio_sm_get_blocking(pio_serdes, sm_rx);
+        uint rxf_lv = pio_sm_get_rx_fifo_level(pio_serdes, sm_rx);
 
-        gpio_put(HW_PINNUM_OUT1, 1);    // For CPU usage monitor
-
-        // Search for SFD pattern
-        sfd_det = false;
-        if ( 0xd5555555 == rx_buf_old                          ) { shift_num =  0; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 31) + (rx_buf_old >>  1) ) { shift_num =  1; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 30) + (rx_buf_old >>  2) ) { shift_num =  2; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 29) + (rx_buf_old >>  3) ) { shift_num =  3; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 28) + (rx_buf_old >>  4) ) { shift_num =  4; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 27) + (rx_buf_old >>  5) ) { shift_num =  5; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 26) + (rx_buf_old >>  6) ) { shift_num =  6; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 25) + (rx_buf_old >>  7) ) { shift_num =  7; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 24) + (rx_buf_old >>  8) ) { shift_num =  8; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 23) + (rx_buf_old >>  9) ) { shift_num =  9; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 22) + (rx_buf_old >> 10) ) { shift_num = 10; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 21) + (rx_buf_old >> 11) ) { shift_num = 11; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 20) + (rx_buf_old >> 12) ) { shift_num = 12; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 19) + (rx_buf_old >> 13) ) { shift_num = 13; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 18) + (rx_buf_old >> 14) ) { shift_num = 14; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 17) + (rx_buf_old >> 15) ) { shift_num = 15; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 16) + (rx_buf_old >> 16) ) { shift_num = 16; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 15) + (rx_buf_old >> 17) ) { shift_num = 17; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 14) + (rx_buf_old >> 18) ) { shift_num = 18; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 13) + (rx_buf_old >> 19) ) { shift_num = 19; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 12) + (rx_buf_old >> 20) ) { shift_num = 20; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 11) + (rx_buf_old >> 21) ) { shift_num = 21; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf << 10) + (rx_buf_old >> 22) ) { shift_num = 22; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  9) + (rx_buf_old >> 23) ) { shift_num = 23; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  8) + (rx_buf_old >> 24) ) { shift_num = 24; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  7) + (rx_buf_old >> 25) ) { shift_num = 25; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  6) + (rx_buf_old >> 26) ) { shift_num = 26; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  5) + (rx_buf_old >> 27) ) { shift_num = 27; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  4) + (rx_buf_old >> 28) ) { shift_num = 28; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  3) + (rx_buf_old >> 29) ) { shift_num = 29; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  2) + (rx_buf_old >> 30) ) { shift_num = 30; sfd_det = true; }
-        if ( 0xd5555555 == (rx_buf <<  1) + (rx_buf_old >> 31) ) { shift_num = 31; sfd_det = true; }
-    
-        if (sfd_det) {
-            multicore_fifo_push_blocking((index << 2) + slot);  // Notify for Core0
-            slot = (slot + 1) & 0x3;
-            index = 0;
-        } else {
-            uint32_t tmp = (rx_buf <<  (32 - shift_num)) + (rx_buf_old >> shift_num);
-            gsram[slot][index] = (tmp << 24) | ((tmp & 0x0000FF00) << 8) | ((tmp & 0x00FF0000) >> 8) | (tmp >> 24);
-            index = (index + 1) & 0x1FF;
+        if (rxf_lv != 0) {
+            gpio_put(HW_PINNUM_OUT1, 1);    // For CPU usage monitor
+            rx_buf = pio_sm_get(pio_serdes, sm_rx);
+            // Search for SFD pattern
+            sfd_det = false;
+            if ( 0xd5555555 == rx_buf_old                          ) { shift_num =  0; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 31) + (rx_buf_old >>  1) ) { shift_num =  1; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 30) + (rx_buf_old >>  2) ) { shift_num =  2; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 29) + (rx_buf_old >>  3) ) { shift_num =  3; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 28) + (rx_buf_old >>  4) ) { shift_num =  4; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 27) + (rx_buf_old >>  5) ) { shift_num =  5; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 26) + (rx_buf_old >>  6) ) { shift_num =  6; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 25) + (rx_buf_old >>  7) ) { shift_num =  7; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 24) + (rx_buf_old >>  8) ) { shift_num =  8; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 23) + (rx_buf_old >>  9) ) { shift_num =  9; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 22) + (rx_buf_old >> 10) ) { shift_num = 10; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 21) + (rx_buf_old >> 11) ) { shift_num = 11; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 20) + (rx_buf_old >> 12) ) { shift_num = 12; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 19) + (rx_buf_old >> 13) ) { shift_num = 13; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 18) + (rx_buf_old >> 14) ) { shift_num = 14; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 17) + (rx_buf_old >> 15) ) { shift_num = 15; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 16) + (rx_buf_old >> 16) ) { shift_num = 16; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 15) + (rx_buf_old >> 17) ) { shift_num = 17; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 14) + (rx_buf_old >> 18) ) { shift_num = 18; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 13) + (rx_buf_old >> 19) ) { shift_num = 19; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 12) + (rx_buf_old >> 20) ) { shift_num = 20; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 11) + (rx_buf_old >> 21) ) { shift_num = 21; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf << 10) + (rx_buf_old >> 22) ) { shift_num = 22; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  9) + (rx_buf_old >> 23) ) { shift_num = 23; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  8) + (rx_buf_old >> 24) ) { shift_num = 24; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  7) + (rx_buf_old >> 25) ) { shift_num = 25; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  6) + (rx_buf_old >> 26) ) { shift_num = 26; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  5) + (rx_buf_old >> 27) ) { shift_num = 27; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  4) + (rx_buf_old >> 28) ) { shift_num = 28; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  3) + (rx_buf_old >> 29) ) { shift_num = 29; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  2) + (rx_buf_old >> 30) ) { shift_num = 30; sfd_det = true; }
+            if ( 0xd5555555 == (rx_buf <<  1) + (rx_buf_old >> 31) ) { shift_num = 31; sfd_det = true; }
+        
+            if (sfd_det) {
+                multicore_fifo_push_blocking((index << 2) + slot);  // Notify for Core0
+                slot = (slot + 1) & 0x3;
+                index = 0;
+            } else {
+                uint32_t tmp = (rx_buf <<  (32 - shift_num)) + (rx_buf_old >> shift_num);
+                gsram[slot][index] = (tmp << 24) | ((tmp & 0x0000FF00) << 8) | ((tmp & 0x00FF0000) >> 8) | (tmp >> 24);
+                index = (index + 1) & 0x1FF;
+            }
+            rx_buf_old = rx_buf;
+            ldm_timer = time_us_32();
+            gpio_put(HW_PINNUM_OUT1, 0);    // For CPU usage monitor
         }
 
-        rx_buf_old = rx_buf;
-
-        gpio_put(HW_PINNUM_OUT1, 0);    // For CPU usage monitor
+        // Detect Link Down
+        if (time_us_32() - ldm_timer > DEF_LINK_TIMEOUT_US) {
+            printf("Link Down...\r\n");
+            ldm_timer = time_us_32();
+        }
     }
 }
